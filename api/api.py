2804939d183db5392b0d6e68a35cc716ecc0e756
Ufuk Ulusoy <ufukulusoy9@gmail.com>
2021-11-12 17:30:29 +0300

    first commit

--- api/api.py
+++ api/api.py
@@ -1,8 +1,17 @@
-import time
-from flask import Flask
+import time,sys
+from flask import Flask,request,render_template
+from flask.helpers import url_for
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
+    
+@app.route('/api/input', methods = ['GET','POST'])
+def get_input():
+    if request.method=="POST":
+        place=request.form
+        return place
+        
--- src/App.js
+++ src/App.js
@@ -1,33 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {useState, useEffect} from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
   //request from frontend to backend
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
+    fetch('/api/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
     });
-  },[]);
-
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p> The current time is {currentTime}. </p>
+        <BrowserRouter>
+          <p> <Link className="App-link" to="/"> Input </Link> | <Link className="App-link" to="/results"> Results </Link></p>
+          <Switch>
+            <Route exact path="/">
+              <br />
+              <form action="/api/input" method="post">
+                <p>Submission count: <input type="number" name="count" /></p>
+                <p>Spawn rate: <input type="number" name="spawn" /></p>
+                <p> <input type="submit" value="Submit" /></p>
+              </form>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+              <p> The current time is {currentTime}. </p>
+            
+            </Route>
+            <Route path="/results">
+              <h1>Results will be displayed here</h1>
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </header>
     </div>
   );
